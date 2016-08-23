b1daa91475c9ae93b5c0295ec4707c14d60a4c60
xuan son tran <xuanson33bk@gmail.com>
2016-08-23 14:00:27 +0700

    + add sample about exporting use ClosedXML

--- ExcelCSharp/Form1.cs
+++ ExcelCSharp/Form1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClosedXML.Excel;
 using System.Windows.Forms;
 
 namespace ExcelCSharp
@@ -15,6 +8,18 @@ namespace ExcelCSharp
         public Form1()
         {
             InitializeComponent();
+            // create new excel workbook
+            var workbook = new XLWorkbook();
+            // set author for this workbook
+            workbook.Author = "www.sontx.in";
+            // create a sheet
+            var worksheet = workbook.Worksheets.Add("new worksheet");
+            // set value for A1 cell in this worksheet
+            worksheet.Cell("A1").Value = "This is value in A1 cell";
+            // export excel file to disk
+            workbook.SaveAs("my_report.xlsx");
+            // clean up
+            workbook.Dispose();
         }
     }
 }
